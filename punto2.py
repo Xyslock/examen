c56478e90ce9aef66eda47ac68e25e8d739f090d
Xyslock <georgemichaeltabordagarces@gmail.com>
2023-09-09 16:50:37 -0500

    cambio

---
+++ punto2.py
@@ -0,0 +1,10 @@
+notas_estudiantes = {
+    'Juan': [3.2, 4.5, 5],
+    'Maria': [4.2, 3.5, 4.3],
+    'Pedro': [3.9, 2.5, 4.8]
+}
+for nombre,nota in notas_estudiantes.items():
+    suma =0
+    for promedio in nota:
+        suma= suma+promedio
+    print(nombre,suma/len(nota))
